090ce5b08f5987f4f2b66fb2597484e5d1ff6a5a
unknown <keith.francis83@gmail.com>
2025-02-25 19:31:02 -0700

    added NotBlank annotation, validation, binging results, and thyme error

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -3,8 +3,11 @@ package com.ltp.gradesubmission;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,8 +32,11 @@ public class GradeController {
     }
 
     @PostMapping("/handleSubmit")
-    public String submitForm(Grade grade) {
+    public String submitForm(@Valid Grade grade, BindingResult result) {
         int index = getGradeIndex(grade.getId());
+
+        if (result.hasErrors()) return "form";
+
         if (index == Constants.NOT_FOUND) {
             studentGrades.add(grade);
         } else {
